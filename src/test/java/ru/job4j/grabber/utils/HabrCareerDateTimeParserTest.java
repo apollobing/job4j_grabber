0f56e05431dfb9696ecda04161296cba6527ce01
apollobing <taras.career@gmail.com>
2023-11-19 12:32:01 +0200

    2.1. Преобразование даты [#289476]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void whenStringContainsCorrectTemplateOfDateTimeThenExpectedEqualsToResult() {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        String dateTime = "2023-11-17T18:15:42+03:00";
+        LocalDateTime expected = LocalDateTime.parse("2023-11-17T18:15:42");
+        assertThat(parser.parse(dateTime)).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStringContainsIncorrectTemplateOfDateTimeThenExpectedIsNotEqualsToResult() {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        String dateTime = "2023-11-17T18:15:42.630";
+        LocalDateTime expected = LocalDateTime.parse("2023-11-17T18:15:42");
+        assertThat(parser.parse(dateTime)).isNotEqualTo(expected);
+    }
+
+}
