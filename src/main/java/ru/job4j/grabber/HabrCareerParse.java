e4efd68c36e4eab21a2d585592c5e1266f410a09
apollobing <taras.career@gmail.com>
2023-11-20 12:12:22 +0200

    refactor: add constant PAGES, add createPost method, change addPost and list methods - 2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -17,6 +17,7 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int PAGES = 5;
     private List<Post> postList = new ArrayList<>();
     private final DateTimeParser dateTimeParser;
 
@@ -35,27 +36,31 @@ public class HabrCareerParse implements Parse {
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {
-            Post post = new Post();
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element dateTimeElement = dateElement.child(0);
-            String vacancyDate = dateTimeElement.attr("datetime");
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String description;
-            try {
-                description = retrieveDescription(
-                        "%s%s".formatted(SOURCE_LINK, linkElement.attr("href")));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            post.setTitle(vacancyName);
-            post.setLink(link);
-            post.setCreated(dateTimeParser.parse(vacancyDate));
-            post.setDescription(description);
+            Post post = createPost(row);
             postList.add(post);
         });
+    }
+
+    private Post createPost(Element row) {
+        Post post = new Post();
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        String vacancyName = titleElement.text();
+        Element dateElement = row.select(".vacancy-card__date").first();
+        Element dateTimeElement = dateElement.child(0);
+        String vacancyDate = dateTimeElement.attr("datetime");
+        String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        String description;
+        try {
+            description = retrieveDescription(link);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        post.setTitle(vacancyName);
+        post.setLink(link);
+        post.setCreated(dateTimeParser.parse(vacancyDate));
+        post.setDescription(description);
+        return post;
     }
 
     public static void main(String[] args) {
@@ -65,7 +70,7 @@ public class HabrCareerParse implements Parse {
 
     @Override
     public List<Post> list(String link) {
-        for (int pageNumber = 1; pageNumber <= 5; pageNumber++) {
+        for (int pageNumber = 1; pageNumber <= PAGES; pageNumber++) {
             try {
                 addPost(link.concat("%s%d%s".formatted(PREFIX, pageNumber, SUFFIX)));
             } catch (IOException e) {
